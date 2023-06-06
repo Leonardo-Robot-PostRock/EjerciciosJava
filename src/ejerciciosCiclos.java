393e23eda6896ed6d04d8c4816d4afa65edc742b
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-06 19:19:52 -0300

    ciclo for en java

---
+++ src/ejerciciosCiclos.java
@@ -0,0 +1,11 @@
+public class ejerciciosCiclos {
+    public static void main(String[] args) {
+        for(int i=0; i<=10; i++){
+            System.out.println("Valor de i " + i);
+        }
+        System.out.println("-------------------");
+        for(int i=10; i>=0; i--){
+            System.out.println("Valor de i " + i);
+        }
+    }
+}
