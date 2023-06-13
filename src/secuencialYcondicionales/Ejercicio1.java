edb5378985a171c865b7cf1e154754f16af5356b
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-13 17:30:30 -0300

    se termina ejercicio 1 calculo de minutos, horas y dias

---
+++ src/secuencialYcondicionales/Ejercicio1.java
@@ -0,0 +1,29 @@
+package secuencialYcondicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int seg = 0;
+        int minutos = 0;
+        int horas = 0;
+        int dias = 0;
+
+        System.out.println("Ingrese la cantidad de segundos");
+        Scanner scanner = new Scanner(System.in); //Create a Scanner
+        seg = scanner.nextInt();
+        System.out.println(seg);
+
+        minutos = seg / 60;
+
+        System.out.println("Son/Es " + minutos + " minuto/s");
+
+        horas = minutos / 60;
+
+        System.out.println("Son/Es " + horas + " hora/s");
+
+        dias = horas / 24;
+
+        System.out.println("Son/Es "+ dias + " dia/s");
+    }
+}
