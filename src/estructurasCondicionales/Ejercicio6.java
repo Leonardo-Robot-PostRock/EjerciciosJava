9a493bc849cb698bed0984c4d64575ea8c8170db
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-21 03:34:39 -0300

    Ejercicio 6 terminado

--- src/estructurasCondicionales/Ejercicio6.java
+++ src/estructurasCondicionales/Ejercicio6.java
@@ -1,9 +1,20 @@
 package estructurasCondicionales;
 
+import javax.swing.*;
+
 public class Ejercicio6 {
     public static void main(String[] args) {
 //        Elaborar un programa que simule el inicio de sesión a un sistema. Si el usuario es: ”ADMIN” y la clave “1234” escribir el siguiente mensaje en pantalla “ACCESO PERMITIDO” caso contrario mostrar el mensaje “ACCESO DENEGADO”.
 
+        String usuario, contrasenia;
 
+        usuario = JOptionPane.showInputDialog("Ingrese usuario");
+        contrasenia = JOptionPane.showInputDialog("Ingrese contraseña");
+
+        if (usuario.equals("ADMIN") && contrasenia.equals("1234")){
+            System.out.println("Acceso Permitido");
+        }else {
+            System.out.println("Acceso Denegado");
+        }
     }
 }
