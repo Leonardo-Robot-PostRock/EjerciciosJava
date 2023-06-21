3b3c20bc767ea360d3c838785ffcf966f7e8cd1e
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-20 21:23:28 -0300

    imprimir un arreglo y cargarlo sacar promedio

--- src/ejemplo2.java
+++ src/ejemplo2.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ejemplo2 {
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ejemplo2 {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[5];
+
+        cargarArreglo(numeros);
+        imprimirArreglo(numeros);
+        System.out.println(promedio(numeros));
+
+    }
+
+    public static void cargarArreglo(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese valor " + i));
+        }
+    }
+
+    public static void imprimirArreglo(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Elemento " + i + " " + arreglo[i]);
+        }
+    }
+
+    public static int promedio(int[] arreglo) {
+        int suma, promedio;
+
+        suma = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma += arreglo[i];
+        }
+
+        promedio = suma / arreglo.length;
+        JOptionPane.showMessageDialog(null, Arrays.toString(arreglo));
+
+        return promedio;
+    }
 }
