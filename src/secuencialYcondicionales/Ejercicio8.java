d2b2c25a1fb9ac920a6a0bc063d0c9a7f8df1e80
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-21 02:19:39 -0300

    Ejercicio 8

---
+++ src/secuencialYcondicionales/Ejercicio8.java
@@ -0,0 +1,19 @@
+package secuencialYcondicionales;
+
+import javax.swing.*;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+//        Escriba un programa en PSeInt que pida al usuario dos números y muestra la “distancia” entre ellos (el valor absoluto de su diferencia, de modo que el resultado sea siempre positivo)
+
+        int num1, num2, distancia;
+
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número"));
+
+        distancia = Math.abs(num1 - num2);
+
+        System.out.println("La distancia es " + distancia);
+
+    }
+}
