4d05323bf5153c40af3d7760401eb45d16c92429
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-25 20:45:49 -0300

    ejercicio 4

--- src/cicloWhile/ejercicio4.java
+++ src/cicloWhile/ejercicio4.java
@@ -1,7 +1,21 @@
 package cicloWhile;
 
+import javax.swing.*;
+
 public class ejercicio4 {
     public static void main(String[] args) {
+//        3) ¿Qué falencia puede encontrar en el siguiente algoritmo? ¿Cómo lo solucionaría?
 
+        int dato;
+        String salir = "0";
+
+        while (!salir.equals("1")) {
+            dato = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un número para conocer su anterior y su siguiente"));
+            JOptionPane.showMessageDialog(null, "NUMERO: " + dato + " ANTERIOR: " + (dato - 1) + " SIGUIENTE: " + (dato + 1));
+
+            System.out.println("Para salir pulse la tecla 1.");
+            salir = JOptionPane.showInputDialog(null, "Si desea repetir la operación, pulse cualquier otra tecla");
+        }
+        System.out.println("Saliendo...");
     }
 }
