cd07f78f3d071b8cdd3df75759fd6384cf82d45c
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-25 17:54:20 -0300

    ejercicio 1 ciclos repetitivos

---
+++ src/ciclosRepetitivos/ejercicio1.java
@@ -0,0 +1,11 @@
+package ciclosRepetitivos;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+//        1) Definir un iterador en 0, incrementarlo y mostrarlo por pantalla hasta que el valor sea 10.
+
+        for (int iterador = 0; iterador <= 10; iterador++) {
+            System.out.println(iterador);
+        }
+    }
+}
