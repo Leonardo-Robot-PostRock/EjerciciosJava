ed0a2d9aa0692360a8efd385fe09fbec451798a8
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-25 18:56:13 -0300

    Ejercicio 7

--- src/ciclosRepetitivos/ejercicio7.java
+++ src/ciclosRepetitivos/ejercicio7.java
@@ -1,7 +1,20 @@
 package ciclosRepetitivos;
 
+import javax.swing.*;
+
 public class ejercicio7 {
     public static void main(String[] args) {
+//        7) Realice un algoritmo que, al Introducir 5 números enteros positivos, devuelva el mayor .
 
+        int num, numMayor;
+        numMayor = 0;
+
+        for (int i = 1; i < 5; i++) {
+            num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el número: " + i));
+            if (num > numMayor) {
+                numMayor = num;
+            }
+        }
+        System.out.println("El número mayor es: " + numMayor);
     }
 }
