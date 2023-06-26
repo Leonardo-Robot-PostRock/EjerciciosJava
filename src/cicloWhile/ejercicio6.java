603fff2fa29d02017c29eb66c0a89849ab11ee57
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-25 21:02:17 -0300

    ejercicio 6

--- src/cicloWhile/ejercicio6.java
+++ src/cicloWhile/ejercicio6.java
@@ -1,2 +1,30 @@
-package cicloWhile;public class ejercicio6 {
+package cicloWhile;
+
+import javax.swing.*;
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+//        5) Desarrolle un algoritmo que solicite al usuario ingresar dos números enteros. Una vez ingresados, calcular la suma, resta, multiplicación y división. Mostrar por pantalla el resultado de cada cálculo. Luego deberá consultar al usuario si desea salir o realizar otro cálculo.
+
+        int num1, num2;
+        String candena, salida;
+        salida = "1";
+
+        while (!salida.equals("0")) {
+            num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer número"));
+            num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo número"));
+
+            System.out.println("La operación: " + num1 + " + " + num2 + " es: " + (num1 + num2));
+            System.out.println("La operación: " + num1 + " - " + num2 + " es: " + (num1 - num2));
+            System.out.println("La operación: " + num1 + " * " + num2 + " es: " + (num1 * num2));
+
+            if (num2 != 0) {
+                System.out.println("La operación: " + num1 + " / " + num2 + " es: " + num1 / num2);
+            } else {
+                System.out.println("No se puede dividir por 0!");
+            }
+            salida = JOptionPane.showInputDialog(null, "Para salir ingrese 0, para calcular nuevamente, cualquier otra tecla");
+        }
+        System.out.println("Fin de la ejecución");
+    }
 }
