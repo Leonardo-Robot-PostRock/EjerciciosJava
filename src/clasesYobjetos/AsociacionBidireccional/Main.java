bb0048ad7a750ef445d396f8689103e048a2bfc7
Leonardo <leonardo38361@gmail.com>
2023-08-30 10:59:35 -0300

    se asocian las clases

--- src/clasesYobjetos/AsociacionBidireccional/Main.java
+++ src/clasesYobjetos/AsociacionBidireccional/Main.java
@@ -1,2 +1,22 @@
-package clasesYobjetos.AsociacionBidireccional;public class Main {
+package clasesYobjetos.AsociacionBidireccional;
+
+public class Main {
+    public static void main(String[] args) {
+        //Una persona quiere abrir una cuenta en un banco (Se creará su legajo y su cuenta simultáneamente).
+        //Deberá proveer toda su información personal y el tipo de cuenta y el saldo inicial
+        Persona per1 = new Persona(17456879, "Juan", "Perez");
+        Cuenta cue1 = new Cuenta(2345, "CA", 3456);
+
+        per1.setCuenta(cue1); //asocio la cuenta a la persona;
+        cue1.setPersona(per1); //asocio la persona a la cuenta;
+
+        System.out.println("Cliente: " + per1.getApellido() + " DNI: " + per1.getDni());
+
+        System.out.println("Nro de Cuenta: " + per1.getCuenta().getId() + " Balance: " + per1.getCuenta().getBalance());
+
+        System.out.println("-----------------------------------");
+
+        System.out.println("Cuenta: " + cue1.getId() + "Tipo: " + cue1.getTipo());
+        System.out.println("Pertenece a: " + cue1.getPersona().getApellido());
+    }
 }
