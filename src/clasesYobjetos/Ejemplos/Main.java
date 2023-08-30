e5a19147cd752d0097957036ca6805512dae52a8
Leonardo <leonardo38361@gmail.com>
2023-08-29 21:11:40 -0300

    creando objetos de tipo Camion

---
+++ src/clasesYobjetos/Ejemplos/Main.java
@@ -0,0 +1,8 @@
+package clasesYobjetos.Ejemplos;
+
+public class Main {
+    public static void main(String[] args) {
+        Camion camionDeCocaCola = new Camion("Scania", 1.5f, "Juan Perez");
+        Camion otroCamion = new Camion("Ford", 1.3f, "María Gomez");
+    }
+}
