85015fa893fca137b019940b95a5e72169b88266
Leonardo <leonardo38361@gmail.com>
2023-08-29 23:21:28 -0300

    clase Domicilio para ejemplo de composicion

--- src/clasesYobjetos/Composicion/Domicilio.java
+++ src/clasesYobjetos/Composicion/Domicilio.java
@@ -1,4 +1,33 @@
 package clasesYobjetos.Composicion;
 
 public class Domicilio {
+    //Declaración de atributos
+
+    private String nombreCalle;
+    private int numero;
+
+    //Constructor vacío de la clase
+    public Domicilio() {}
+
+    //Constructor sobrecargado de la clase
+    public Domicilio(String pnombreCalle, int pnumero) {
+        this.nombreCalle = pnombreCalle;
+        this.numero = pnumero;
+    }
+
+    public String getNombreCalle() {
+        return this.nombreCalle;
+    }
+
+    public void setNombreCalle(String val) {
+        this.nombreCalle = val;
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(int val) {
+        this.numero = val;
+    }
 }
