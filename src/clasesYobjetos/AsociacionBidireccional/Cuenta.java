7f7e71d9fe59159924902531babe0101bea9a2d8
Leonardo <leonardo38361@gmail.com>
2023-08-30 10:55:38 -0300

    Se crea clase Cuenta con su metodos. Se crea variable de instancia de tipo Persona

--- src/clasesYobjetos/AsociacionBidireccional/Cuenta.java
+++ src/clasesYobjetos/AsociacionBidireccional/Cuenta.java
@@ -1,2 +1,47 @@
-package clasesYobjetos.AsociacionBidireccional;public class Cuenta {
+package clasesYobjetos.AsociacionBidireccional;
+
+public class Cuenta {
+    private int id;
+    private String tipo;
+    private double balance;
+
+    private Persona persona;
+    public Cuenta(){}
+    public Cuenta(int id, String tipo, double balance) {
+        this.id = id;
+        this.tipo = tipo;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
 }
