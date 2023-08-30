bc87c89851bb8a267e885a6e9e5cc506d02e056c
Leonardo <leonardo38361@gmail.com>
2023-08-29 23:24:00 -0300

    se crea clase persona con relacion con Domicilio por composicion

--- src/clasesYobjetos/Composicion/Persona.java
+++ src/clasesYobjetos/Composicion/Persona.java
@@ -1,4 +1,42 @@
 package clasesYobjetos.Composicion;
 
 public class Persona {
+    private String nombre;
+    private String apellido;
+
+    //declaración de la variable de referencia del tipo Domicilio gracias a ella podremos navegar hacia la clase Domicilio
+
+    private Domicilio mDomicilio;
+
+    public Persona(String pnombre, String papellido, String calle, int numero) {
+        this.nombre = pnombre;
+        this.apellido = papellido;
+        mDomicilio = new Domicilio(calle, numero);
+    }
+
+    //Declaración de los métodos get y set para cada atributo de la clase
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String val) {
+        this.apellido = val;
+    }
+
+    public Domicilio getDomicilio() {
+        return mDomicilio;
+    }
+
+    public void setDomicilio(Domicilio val) {
+        this.mDomicilio = val;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String val) {
+        this.nombre = val;
+    }
 }
