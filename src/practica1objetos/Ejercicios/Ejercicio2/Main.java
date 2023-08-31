f1c3ba4e08c892b90109c01d8002c69ae0290595
Leonardo <leonardo38361@gmail.com>
2023-08-31 03:05:56 -0300

    Ejercicio 2 completado

--- src/practica1objetos/Ejercicios/Ejercicio2/Main.java
+++ src/practica1objetos/Ejercicios/Ejercicio2/Main.java
@@ -1,2 +1,35 @@
-package practica1objetos.Ejercicios.Ejercicio2;public class Main {
+package practica1objetos.Ejercicios.Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Aylen", 19, 34321323, 60, 1.60);
+        Persona persona2 = new Persona("Fernando", 38, 60, 1.70);
+
+        System.out.println(persona1.getNombre() + ":");
+        int resultadoIMC = persona1.calcularIMC(persona1.getPeso(), persona1.getAltura());
+
+        if (resultadoIMC == 1) {
+            System.out.println("El IMC indica bajo de peso");
+        } else if (resultadoIMC == 0) {
+            System.out.println("El IMC indica peso ideal");
+        } else {
+            System.out.println("El IMC indica sobrepeso");
+        }
+
+        boolean esMayor = persona1.esMayorDeEdad();
+
+        if (esMayor) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("No es mayor de edad");
+        }
+
+        System.out.println(persona1.getDni());
+
+        System.out.println("------------------");
+
+        System.out.println(persona2.getNombre() + ":");
+        persona2.imprimir();
+
+    }
 }
--- src/practica1objetos/Ejercicios/Ejercicio2/Persona.java
+++ src/practica1objetos/Ejercicios/Ejercicio2/Persona.java
@@ -1,2 +1,110 @@
-package practica1objetos.Ejercicios.Ejercicio2;public class Persona {
+package practica1objetos.Ejercicios.Ejercicio2;
+
+import java.util.Random;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private char letraDNI;
+    private int dni;
+    private double peso;
+    private double altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona(String nombre, int edad, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = generaDNI();
+        this.letraDNI = generaLetraDNI();
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public Persona(String nombre, int edad, int dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int calcularIMC(double peso, double altura) {
+        double indiceMasaCorporal = (peso / (Math.pow(altura, 2)));
+        if (indiceMasaCorporal < 20) {
+            return -1;
+        } else if (indiceMasaCorporal >= 20 && indiceMasaCorporal <= 25) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad() {
+        return this.edad >= 18;
+    }
+
+    private int generaDNI() {
+        Random random = new Random();
+        return random.nextInt(90000000) + 10000000;
+    }
+
+    private char generaLetraDNI() {
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        return letras.charAt(this.dni % 23);
+    }
+
+    public void imprimir() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Edad: " + this.edad);
+        System.out.println("DNI: " + this.letraDNI + this.dni);
+        System.out.println("Peso: " + this.peso);
+        System.out.println("Altura: " + this.altura);
+    }
 }
