61766c6163af9a8510a30ca7d817838babc1037c
Leonardo <leonardo38361@gmail.com>
2023-09-05 20:18:49 -0300

    Implementacion de método volar

--- src/clasesYobjetos/EjercicioHerencia2/Ave.java
+++ src/clasesYobjetos/EjercicioHerencia2/Ave.java
@@ -1,2 +1,9 @@
-package clasesYobjetos.EjercicioHerencia2;public class Ave {
+package clasesYobjetos.EjercicioHerencia2;
+
+public class Ave implements Volador {
+
+    @Override
+    public void volar() {
+        System.out.println("Yo vuelo con mis alas");
+    }
 }
