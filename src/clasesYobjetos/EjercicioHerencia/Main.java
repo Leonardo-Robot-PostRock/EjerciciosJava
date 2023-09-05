a035e459d9a7877999ecd82117862f7fde00647a
Leonardo <leonardo38361@gmail.com>
2023-09-05 19:58:40 -0300

    Se crean objetos

--- src/clasesYobjetos/EjercicioHerencia/Main.java
+++ src/clasesYobjetos/EjercicioHerencia/Main.java
@@ -2,6 +2,14 @@ package clasesYobjetos.EjercicioHerencia;
 
 public class Main {
     public static void main(String[] args) {
+        Alumno a1 = new Alumno("Pepe", "Honguito", 16, 1234);
 
+        Profesor p1 = new Profesor("Tito", "Perez", 40, "Profesor");
+
+        System.out.println("El alumno " + a1.getNombre());
+        a1.andar();
+
+        System.out.println("El profesor " + p1.getNombre());
+        p1.andar();
     }
 }
