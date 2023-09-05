a185d2a55c6734f5fe81998c89b18278f72bc74b
Leonardo <leonardo38361@gmail.com>
2023-09-05 20:00:17 -0300

    sobrescribir el método andar

--- src/clasesYobjetos/EjercicioHerencia/Profesor.java
+++ src/clasesYobjetos/EjercicioHerencia/Profesor.java
@@ -1,4 +1,29 @@
 package clasesYobjetos.EjercicioHerencia;
 
-public class Profesor {
+import clasesYobjetos.Herencia.Persona;
+
+public class Profesor extends Persona {
+    private String title;
+
+    public Profesor(String title) {
+        this.title = title;
+    }
+
+    public Profesor(String nombre, String apellido, long dni, String title) {
+        super(nombre, apellido, dni);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void andar() {
+        System.out.println("Soy un profesor que camina");
+    }
 }
