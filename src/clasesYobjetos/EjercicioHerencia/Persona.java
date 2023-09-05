c037214c501ee965f8c5c87d7bc8c3ef80967abe
Leonardo <leonardo38361@gmail.com>
2023-09-05 19:59:37 -0300

    implementación de método abstracto

--- src/clasesYobjetos/EjercicioHerencia/Persona.java
+++ src/clasesYobjetos/EjercicioHerencia/Persona.java
@@ -11,5 +11,29 @@ public abstract class Persona {
         this.edad = edad;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     public abstract void andar();
 }
