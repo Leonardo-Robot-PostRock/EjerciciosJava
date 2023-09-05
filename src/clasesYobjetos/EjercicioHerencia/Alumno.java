0c03a72f9d9a1a1647384bb21c3740cd834b9eb6
Leonardo <leonardo38361@gmail.com>
2023-09-05 19:40:49 -0300

    se sobreescribe método que viene de la clase abstracta

--- src/clasesYobjetos/EjercicioHerencia/Alumno.java
+++ src/clasesYobjetos/EjercicioHerencia/Alumno.java
@@ -1,4 +1,23 @@
 package clasesYobjetos.EjercicioHerencia;
 
-public class Alumno {
+public class Alumno extends Persona {
+    private int legajo;
+
+    public Alumno(String nombre, String apellido, int edad, int legajo) {
+        super(nombre, apellido, edad);
+        this.legajo = legajo;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    @Override
+    public void andar() {
+        System.out.println("Soy un alumno que corre");
+    }
 }
