d21a0c75888660de34bb75997fb45dcdb2b5a494
Leonardo <leonardo38361@gmail.com>
2023-09-06 00:05:39 -0300

    Ejercicio 3

--- src/practica1objetos/Ejercicios/Ejercicio3/Serie.java
+++ src/practica1objetos/Ejercicios/Ejercicio3/Serie.java
@@ -1,2 +1,66 @@
-package practica1objetos.Ejercicios.Ejercicio3;public class Serie {
+package practica1objetos.Ejercicios.Ejercicio3;
+
+public class Serie {
+    private String titulo;
+    private int numeroDeTemporadas;
+    private boolean entregado;
+    private String genero;
+    private String creador;
+
+    public Serie() {
+        this.numeroDeTemporadas = 3;
+        this.entregado = false;
+    }
+
+    public Serie(String titulo, String creador) {
+        this.titulo = titulo;
+        this.creador = creador;
+    }
+
+    public Serie(String titulo, int numeroDeTemporadas, String genero, String creador) {
+        this.titulo = titulo;
+        this.numeroDeTemporadas = numeroDeTemporadas;
+        this.genero = genero;
+        this.creador = creador;
+    }
+
+    public Serie(String titulo, int numeroDeTemporadas, boolean entregado, String genero, String creador) {
+        this.titulo = titulo;
+        this.numeroDeTemporadas = numeroDeTemporadas;
+        this.entregado = entregado;
+        this.genero = genero;
+        this.creador = creador;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumeroDeTemporadas() {
+        return numeroDeTemporadas;
+    }
+
+    public void setNumeroDeTemporadas(int numeroDeTemporadas) {
+        this.numeroDeTemporadas = numeroDeTemporadas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
 }
