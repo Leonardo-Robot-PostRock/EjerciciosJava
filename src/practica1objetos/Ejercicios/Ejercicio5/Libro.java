ea8cdf5218ec18f93277ae6d0c9c1c62a1daf499
Leonardo <leonardo38361@gmail.com>
2023-09-06 00:38:38 -0300

    Ejercicio 5

--- src/practica1objetos/Ejercicios/Ejercicio5/Libro.java
+++ src/practica1objetos/Ejercicios/Ejercicio5/Libro.java
@@ -1,5 +1,70 @@
 package practica1objetos.Ejercicios.Ejercicio5;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private int ejemplaresTotales;
+    private int ejemplaresDisponibles;
 
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, int ejemplaresTotales) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ejemplaresTotales = ejemplaresTotales;
+        this.ejemplaresDisponibles = ejemplaresTotales;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getEjemplaresDisponibles() {
+        return ejemplaresDisponibles;
+    }
+
+    public void setEjemplaresDisponibles(int ejemplaresDisponibles) {
+        this.ejemplaresDisponibles = ejemplaresDisponibles;
+    }
+
+    public int getEjemplaresTotales() {
+        return ejemplaresTotales;
+    }
+
+    public void setEjemplaresTotales(int ejemplaresTotales) {
+        this.ejemplaresTotales = ejemplaresTotales;
+    }
+
+    public boolean prestar() {
+        if (this.ejemplaresDisponibles > 0) {
+            this.ejemplaresDisponibles--;
+            return true;
+        } else {
+            System.out.println("No quedan libros disponibles");
+            return false;
+        }
+    }
+
+    public boolean devolver() {
+        if (this.ejemplaresDisponibles < this.ejemplaresTotales) {
+            this.ejemplaresDisponibles++;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/practica1objetos/Ejercicios/Ejercicio5/Main.java
+++ src/practica1objetos/Ejercicios/Ejercicio5/Main.java
@@ -2,6 +2,19 @@ package practica1objetos.Ejercicios.Ejercicio5;
 
 public class Main {
     public static void main(String[] args) {
+        Libro libro1 = new Libro("Harry Potter", "J. K. Rowling", 3);
+
+        System.out.println("Libro: " + libro1.getTitulo());
+        System.out.println("El autor es: " + libro1.getAutor());
+        System.out.println("Nº de ejemplares: " + libro1.getEjemplaresTotales());
+
+        System.out.println("Se procede a prestar un libro: ");
+        libro1.prestar();
+        libro1.prestar();
+        libro1.prestar();
+        libro1.prestar();
+        System.out.println("Numero de ejemplares restantes: " + libro1.getEjemplaresDisponibles());
+
 
     }
 }
