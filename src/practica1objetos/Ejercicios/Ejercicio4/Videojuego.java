1e9cc8ceef110ee427ca7c907d12d86b8e5d6504
Leonardo <leonardo38361@gmail.com>
2023-09-06 00:06:03 -0300

    Ejercicio 4

--- src/practica1objetos/Ejercicios/Ejercicio4/Videojuego.java
+++ src/practica1objetos/Ejercicios/Ejercicio4/Videojuego.java
@@ -1,2 +1,58 @@
-package practica1objetos.Ejercicios.Ejercicio4;public class Videojuego {
+package practica1objetos.Ejercicios.Ejercicio4;
+
+public class Videojuego {
+    private String titulo;
+    private int horasEstimadas;
+    private boolean entregado;
+    private String genero;
+    private String empresa;
+
+    public Videojuego() {
+        this.horasEstimadas = 10;
+        this.entregado = false;
+    }
+
+    public Videojuego(String titulo, int horasEstimadas) {
+        this.titulo = titulo;
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public Videojuego(String titulo, int horasEstimadas, String genero, String empresa) {
+        this.titulo = titulo;
+        this.horasEstimadas = horasEstimadas;
+        this.genero = genero;
+        this.empresa = empresa;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getHorasEstimadas() {
+        return horasEstimadas;
+    }
+
+    public void setHorasEstimadas(int horasEstimadas) {
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
 }
