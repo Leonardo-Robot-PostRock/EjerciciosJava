8e90620c24f114624ba2cbd63706df5d44fc62c1
Leonardo <leonardo38361@gmail.com>
2023-09-18 15:55:40 -0300

    se crea clase Memoria

--- src/Practica2Relaciones/Ejercicios/Ejercicio2/Memoria.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio2/Memoria.java
@@ -1,2 +1,30 @@
-package Practica2Relaciones.Ejercicios.Ejercicio2;public class Memoria {
+package Practica2Relaciones.Ejercicios.Ejercicio2;
+
+public class Memoria {
+    private int capacidadGB;
+    private String tipo;
+
+    public Memoria() {
+    }
+
+    public Memoria(int capacidadGB, String tipo) {
+        this.capacidadGB = capacidadGB;
+        this.tipo = tipo;
+    }
+
+    public int getCapacidadGB() {
+        return capacidadGB;
+    }
+
+    public void setCapacidadGB(int capacidadGB) {
+        this.capacidadGB = capacidadGB;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
