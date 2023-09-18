2fb9baeb1c79cd99e1365a07cdd6e1e5c9f16b51
Leonardo <leonardo38361@gmail.com>
2023-09-18 17:42:00 -0300

    Se crea clase caja de cambios

--- src/Practica2Relaciones/Ejercicios/Ejercicio3/CajaDeCambios.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio3/CajaDeCambios.java
@@ -1,2 +1,30 @@
-package Practica2Relaciones.Ejercicios.Ejercicio3;public class CajaDeCambios {
+package Practica2Relaciones.Ejercicios.Ejercicio3;
+
+public class CajaDeCambios {
+    private int numMarchas;
+    private String tipo;
+
+    public CajaDeCambios() {
+    }
+
+    public CajaDeCambios(int numMarchas, String tipo){
+        this.numMarchas = numMarchas;
+        this.tipo = tipo;
+    }
+
+    public int getNumMarchas() {
+        return numMarchas;
+    }
+
+    public void setNumMarchas(int numMarchas) {
+        this.numMarchas = numMarchas;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
