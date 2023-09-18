1de07a86734076644bf916c3832e8910b9a05a03
Leonardo <leonardo38361@gmail.com>
2023-09-18 15:57:18 -0300

    se crea relacion de agregacion de computadora con cada uno de los componentes y dispositivos

--- src/Practica2Relaciones/Ejercicios/Ejercicio2/Main.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio2/Main.java
@@ -1,2 +1,13 @@
-package Practica2Relaciones.Ejercicios.Ejercicio2;public class Main {
+package Practica2Relaciones.Ejercicios.Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        CPU cpu = new CPU("Intel i7", 3.2);
+        Memoria memoria = new Memoria(16, "DDR4");
+        Perifericos perifericos = new Perifericos("Teclado Logitech", "USB");
+
+        Computadora miComputadora = new Computadora();
+        miComputadora.agregarDispositivosComponentes(cpu, memoria, perifericos);
+        miComputadora.imprimir();
+    }
 }
