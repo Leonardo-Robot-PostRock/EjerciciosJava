94cfab8746f247c6836d48cc68d823c2dc6f3984
Leonardo <leonardo38361@gmail.com>
2023-09-18 17:45:03 -0300

    se crea la relación por composicion

--- src/Practica2Relaciones/Ejercicios/Ejercicio3/Main.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio3/Main.java
@@ -1,2 +1,13 @@
-package Practica2Relaciones.Ejercicios.Ejercicio3;public class Main {
+package Practica2Relaciones.Ejercicios.Ejercicio3;
+import practica1objetos.EjercicioModelo.AutoComposicion;
+
+public class Main {
+    public static void main(String[] args) {
+        Motor motor = new Motor("Gasolina", 2000);
+        CajaDeCambios cajaDeCambios = new CajaDeCambios(6, "Automática");
+        Llantas llantas = new Llantas("Michelin", 18);
+
+        AutoComposicion miAuto = new AutoComposicion(cajaDeCambios, llantas, motor);
+        miAuto.imprimir();
+    }
 }
