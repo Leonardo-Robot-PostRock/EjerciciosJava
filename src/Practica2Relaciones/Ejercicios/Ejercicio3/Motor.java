b4eadc95d1b75a48f78ceeb6040db7a3ecd015db
Leonardo <leonardo38361@gmail.com>
2023-09-18 17:45:30 -0300

    se crea clase Motor

--- src/Practica2Relaciones/Ejercicios/Ejercicio3/Motor.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio3/Motor.java
@@ -1,2 +1,30 @@
-package Practica2Relaciones.Ejercicios.Ejercicio3;public class Motor {
+package Practica2Relaciones.Ejercicios.Ejercicio3;
+
+public class Motor {
+    private String tipo;
+    private double cilindrada;
+
+    public Motor() {
+    }
+
+    public Motor(String tipo, double cilindrada){
+        this.tipo = tipo;
+        this.cilindrada = cilindrada;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
 }
