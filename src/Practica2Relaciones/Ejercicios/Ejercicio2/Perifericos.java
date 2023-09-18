f9e14d80b778a998c6e46847c2b639dbe9cdd0ef
Leonardo <leonardo38361@gmail.com>
2023-09-18 15:56:02 -0300

    se crea clase perifericos

--- src/Practica2Relaciones/Ejercicios/Ejercicio2/Perifericos.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio2/Perifericos.java
@@ -1,2 +1,30 @@
-package Practica2Relaciones.Ejercicios.Ejercicio2;public class Perifericos {
+package Practica2Relaciones.Ejercicios.Ejercicio2;
+
+public class Perifericos {
+    private String nombre;
+    private String tipo;
+
+    public Perifericos() {
+    }
+
+    public Perifericos(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
