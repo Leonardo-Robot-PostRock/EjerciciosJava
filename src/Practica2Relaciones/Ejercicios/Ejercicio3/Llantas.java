d074468f51a45f72e98c0774f9e3c0ac92b7c11b
Leonardo <leonardo38361@gmail.com>
2023-09-18 17:42:42 -0300

    se crea la clase Lllantas

--- src/Practica2Relaciones/Ejercicios/Ejercicio3/Llantas.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio3/Llantas.java
@@ -1,2 +1,30 @@
-package Practica2Relaciones.Ejercicios.Ejercicio3;public class Llantas {
+package Practica2Relaciones.Ejercicios.Ejercicio3;
+
+public class Llantas {
+    private String marca;
+    private int diametroPulgadas;
+
+    public Llantas() {
+    }
+
+    public Llantas(String marca, int diametroPulgadas) {
+        this.marca = marca;
+        this.diametroPulgadas = diametroPulgadas;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getDiametroPulgadas() {
+        return diametroPulgadas;
+    }
+
+    public void setDiametroPulgadas(int diametroPulgadas) {
+        this.diametroPulgadas = diametroPulgadas;
+    }
 }
