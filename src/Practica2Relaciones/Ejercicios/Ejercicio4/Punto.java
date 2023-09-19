c41d11a4ad29ef5c95a276ed174363ebe885e8b4
Leonardo <leonardo38361@gmail.com>
2023-09-19 18:27:43 -0300

    se crea la clase Punto

--- src/Practica2Relaciones/Ejercicios/Ejercicio4/Punto.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio4/Punto.java
@@ -1,2 +1,30 @@
-package Practica2Relaciones.Ejercicios.Ejercicio4;public class Punto {
+package Practica2Relaciones.Ejercicios.Ejercicio4;
+
+public class Punto {
+    private int x;
+    private int y;
+
+    public Punto() {
+    }
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
