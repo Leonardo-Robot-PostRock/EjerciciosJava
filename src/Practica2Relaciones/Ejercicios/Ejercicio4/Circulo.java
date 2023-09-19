f6a7a5096b1fa08caeab950c0ac5ee6b7a262342
Leonardo <leonardo38361@gmail.com>
2023-09-19 18:26:39 -0300

    Se crea la clase circulo

--- src/Practica2Relaciones/Ejercicios/Ejercicio4/Circulo.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio4/Circulo.java
@@ -1,2 +1,24 @@
-package Practica2Relaciones.Ejercicios.Ejercicio4;public class Circulo {
+package Practica2Relaciones.Ejercicios.Ejercicio4;
+
+public class Circulo {
+    private int radio;
+    private final Punto centro;
+
+    public Circulo(int radio, Punto centro) {
+        this.radio = radio;
+        this.centro = centro;
+    }
+
+    public int getRadio() {
+        return radio;
+    }
+
+    public void setRadio(int radio) {
+        this.radio = radio;
+    }
+
+    public void imprimir() {
+        System.out.println("El centro del círculo es la coordenada (" + centro.getX() + "," + centro.getY() + ")" + " y tiene un radio = " + radio);
+    }
+
 }
