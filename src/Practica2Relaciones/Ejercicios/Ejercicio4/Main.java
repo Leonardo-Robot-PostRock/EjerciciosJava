561975aecb9e5ae25d9e7b30be2f8df259af10d7
Leonardo <leonardo38361@gmail.com>
2023-09-19 18:27:20 -0300

    se prueban las clases Punto y circulo

--- src/Practica2Relaciones/Ejercicios/Ejercicio4/Main.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio4/Main.java
@@ -1,2 +1,10 @@
-package Practica2Relaciones.Ejercicios.Ejercicio4;public class Main {
+package Practica2Relaciones.Ejercicios.Ejercicio4;
+
+public class Main {
+    public static void main(String[] args) {
+        Punto punto1 = new Punto(2,3);
+        Circulo circulo1 = new Circulo(5, punto1);
+
+        circulo1.imprimir();
+    }
 }
