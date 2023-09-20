cf2bab74d79656116cf1a24ba6220d8c4707050e
Leonardo <leonardo38361@gmail.com>
2023-09-20 00:18:17 -0300

    Se crea clase Poligono

--- src/Practica2Relaciones/Ejercicios/Ejercicio5/Poligono.java
+++ src/Practica2Relaciones/Ejercicios/Ejercicio5/Poligono.java
@@ -1,2 +1,41 @@
-package Practica2Relaciones.Ejercicios.Ejercicio5;public class Poligono {
+package Practica2Relaciones.Ejercicios.Ejercicio5;
+
+import Practica2Relaciones.Ejercicios.Ejercicio4.Punto;
+
+import java.util.ArrayList;
+
+public class Poligono {
+    private String nombre;
+    ArrayList<Punto> puntos = new ArrayList<>();
+
+    public Poligono(String nombre, ArrayList<Punto> puntos) {
+        if (puntos.size() < 3) {
+            throw new IllegalArgumentException("Un polígono debe tener al menos 3 puntos");
+        }
+        this.nombre = nombre;
+        this.puntos = puntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Punto> getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(ArrayList<Punto> puntos) {
+        this.puntos = puntos;
+    }
+
+    public void mostrarPuntos() {
+        System.out.println("Puntos del polígono:");
+        for (Punto punto : puntos) {
+            System.out.println("Punto (" + punto.getX() + ", " + punto.getY() + ")");
+        }
+    }
 }
