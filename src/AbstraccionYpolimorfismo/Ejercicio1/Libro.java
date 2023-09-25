46b58240af2190f560cfcf3e68f54aea86fdabcf
Leonardo <leonardo38361@gmail.com>
2023-09-25 16:08:40 -0300

    constructor de la clase

--- src/AbstraccionYpolimorfismo/Ejercicio1/Libro.java
+++ src/AbstraccionYpolimorfismo/Ejercicio1/Libro.java
@@ -1,2 +1,16 @@
-package AbstraccionYpolimorfismo.Ejercicio1;public class Libro {
+package AbstraccionYpolimorfismo.Ejercicio1;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+
+
+
 }
