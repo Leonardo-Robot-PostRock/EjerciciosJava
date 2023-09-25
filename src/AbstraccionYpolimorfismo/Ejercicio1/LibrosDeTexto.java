55c84b38f83b7a457267b4df4ca2e542af60b58e
Leonardo <leonardo38361@gmail.com>
2023-09-25 16:32:38 -0300

    Clase LibrosDeTexto

--- src/AbstraccionYpolimorfismo/Ejercicio1/LibrosDeTexto.java
+++ src/AbstraccionYpolimorfismo/Ejercicio1/LibrosDeTexto.java
@@ -2,8 +2,26 @@ package AbstraccionYpolimorfismo.Ejercicio1;
 
 public class LibrosDeTexto extends Libro {
     private String editorial;
-    public LibrosDeTexto(String titulo, String autor,String editorial) {
+    private Novela novela;
+    public LibrosDeTexto(String titulo, String autor,String editorial, String genero) {
         super(titulo, autor);
         this.editorial = editorial;
+        Novela novela = new Novela(genero);
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public Novela getNovela() {
+        return novela;
+    }
+
+    public void setNovela(Novela novela) {
+        this.novela = novela;
     }
 }
