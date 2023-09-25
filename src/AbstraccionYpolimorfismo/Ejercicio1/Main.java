5669e1e3618ce2c54da7904aeb04968cd4e944ac
Leonardo <leonardo38361@gmail.com>
2023-09-25 16:50:09 -0300

    se prueba el metodo

--- src/AbstraccionYpolimorfismo/Ejercicio1/Main.java
+++ src/AbstraccionYpolimorfismo/Ejercicio1/Main.java
@@ -1,2 +1,9 @@
-package AbstraccionYpolimorfismo.Ejercicio1;public class Main {
+package AbstraccionYpolimorfismo.Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Libro libro = new LibrosDeTexto("Mi planta de Naranja Lima", "Gabriel García Marquez", 5000,"Planeta", "Drama");
+
+        libro.imprimir();
+    }
 }
