6f2bbadc9bb36c2682db50d0c1b1c65d092441a1
Leonardo <leonardo38361@gmail.com>
2023-09-25 16:33:19 -0300

    se agrega clase novela

--- src/AbstraccionYpolimorfismo/Ejercicio1/Novela.java
+++ src/AbstraccionYpolimorfismo/Ejercicio1/Novela.java
@@ -1,2 +1,20 @@
-package AbstraccionYpolimorfismo.Ejercicio1;public class Novela {
+package AbstraccionYpolimorfismo.Ejercicio1;
+
+public class Novela {
+    private String genero;
+
+    public Novela(String genero) {
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void imprimir(){
+        System.out.println("Genero de novela: " + this.genero);
+    }
 }
