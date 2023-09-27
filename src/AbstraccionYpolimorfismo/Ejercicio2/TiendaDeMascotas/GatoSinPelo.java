ce9e7b395518e91c63afe1fc8afc759784f8c337
Leonardo <leonardo38361@gmail.com>
2023-09-26 23:43:36 -0300

    clase Gatos sin Pelo agregada

--- src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/GatoSinPelo.java
+++ src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/GatoSinPelo.java
@@ -1,2 +1,10 @@
-package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;public class GatoSinPelo {
+package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;
+
+public class GatoSinPelo extends Gatos{
+    private String raza;
+
+    public GatoSinPelo(String nombre, int edad, String color, double alturaDesalto, double longitudDeSalto, String raza) {
+        super(nombre, edad, color, alturaDesalto, longitudDeSalto);
+        this.raza = raza;
+    }
 }
