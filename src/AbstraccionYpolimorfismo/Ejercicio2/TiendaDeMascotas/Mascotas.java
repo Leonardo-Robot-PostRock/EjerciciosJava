5495af5e2a20b6732e47ece5c1a1061e7f63085c
Leonardo <leonardo38361@gmail.com>
2023-09-26 23:44:38 -0300

    clase principal agregada

--- src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/Mascotas.java
+++ src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/Mascotas.java
@@ -1,2 +1,40 @@
-package AbstraccionYpolimorfismo.Ejercicio2;public class Mascotas {
+package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;
+
+public abstract class Mascotas {
+    private String nombre;
+    private int edad;
+    private String color;
+
+    public Mascotas(String nombre, int edad, String color) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract void sonido();
+
 }
