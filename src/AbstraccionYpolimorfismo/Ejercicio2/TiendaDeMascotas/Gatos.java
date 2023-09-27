e5f9748e265685ccaf20568cf2ec0655082f582b
Leonardo <leonardo38361@gmail.com>
2023-09-26 23:43:13 -0300

    clase Gatos agregada

--- src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/Gatos.java
+++ src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/Gatos.java
@@ -1,2 +1,33 @@
-package AbstraccionYpolimorfismo.Ejercicio2;public class Gatos {
+package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;
+
+public class Gatos extends Mascotas{
+    private double alturaDesalto;
+    private double longitudDeSalto;
+
+    public Gatos(String nombre, int edad, String color, double alturaDesalto, double longitudDeSalto) {
+        super(nombre, edad, color);
+        this.alturaDesalto = alturaDesalto;
+        this.longitudDeSalto = longitudDeSalto;
+    }
+
+    public double getAlturaDesalto() {
+        return alturaDesalto;
+    }
+
+    public void setAlturaDesalto(double alturaDesalto) {
+        this.alturaDesalto = alturaDesalto;
+    }
+
+    public double getLongitudDeSalto() {
+        return longitudDeSalto;
+    }
+
+    public void setLongitudDeSalto(double longitudDeSalto) {
+        this.longitudDeSalto = longitudDeSalto;
+    }
+
+    @Override
+    public void sonido() {
+        System.out.println("Los gatos maúllan");
+    }
 }
