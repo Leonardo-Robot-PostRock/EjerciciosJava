f6293e33b80319a6f49f1cf3b75dd37edfec64cd
Leonardo <leonardo38361@gmail.com>
2023-09-26 22:24:31 -0300

    Corrección de prueba de objetos

--- src/AbstraccionYpolimorfismo/Ejercicio1/Main.java
+++ src/AbstraccionYpolimorfismo/Ejercicio1/Main.java
@@ -2,8 +2,10 @@ package AbstraccionYpolimorfismo.Ejercicio1;
 
 public class Main {
     public static void main(String[] args) {
-        Novela novela = new Novela("Drama");
-        Libro libro = new LibrosDeTexto("Mi planta de Naranja Lima", "Gabriel García Marquez", 5000,"UNCUYO", "Introducción a Lengua y Literatura", novela.getGenero());
+        Novela novela = new Novela("Mi planta de Naranja Lima", "Gabriel García Marquez", 5000, "Introducción a Lengua y Literatura");
+        Libro libro = new LibrosDeTexto("Learn PHP", "Juan García",6000, "UTN Regional Mendoza", "Curso intensivo de PHP");
         libro.imprimir();
+        System.out.println("--------------");
+        novela.imprimir();
     }
 }
