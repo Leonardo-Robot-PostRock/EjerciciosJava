e4ed0ee4201a876de17d1226dcf5890c035430a8
Leonardo <leonardo38361@gmail.com>
2023-09-27 02:37:32 -0300

    Se pruban los metodos con las instancias

--- src/AbstraccionYpolimorfismo/Ejercicio3/Main.java
+++ src/AbstraccionYpolimorfismo/Ejercicio3/Main.java
@@ -1,4 +1,16 @@
 package AbstraccionYpolimorfismo.Ejercicio3;
 
 public class Main {
+    public static void main(String[] args) {
+        Fraccion frac1 = new Fraccion(2,4);
+        Fraccion frac2 = new Fraccion(3, 4);
+
+        System.out.println(frac1.sumar(frac2));
+        System.out.println(frac1.restar(frac2));
+        System.out.println(frac1.dividir(frac2));
+        System.out.println(frac1.multiplicar(frac2));
+        System.out.println(frac1.equals(frac2));
+
+
+    }
 }
