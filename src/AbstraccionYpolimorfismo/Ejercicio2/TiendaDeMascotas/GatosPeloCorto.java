e59aed713ebfc118d28176479c281cabc31e1828
Leonardo <leonardo38361@gmail.com>
2023-09-26 23:44:01 -0300

    clase agregada

--- src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/GatosPeloCorto.java
+++ src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/GatosPeloCorto.java
@@ -1,2 +1,10 @@
-package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;public class GatosPeloCorto {
+package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;
+
+public class GatosPeloCorto extends Gatos{
+    private String raza;
+
+    public GatosPeloCorto(String nombre, int edad, String color, double alturaDesalto, double longitudDeSalto, String raza) {
+        super(nombre, edad, color, alturaDesalto, longitudDeSalto);
+        this.raza = raza;
+    }
 }
