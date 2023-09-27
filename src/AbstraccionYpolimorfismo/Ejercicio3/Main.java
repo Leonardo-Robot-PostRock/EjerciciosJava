cfdbc02f92d080ab2c7b9b66b1c948f811969323
Leonardo <leonardo38361@gmail.com>
2023-09-27 02:49:43 -0300

    se crea nuevo objeto

--- src/AbstraccionYpolimorfismo/Ejercicio3/Main.java
+++ src/AbstraccionYpolimorfismo/Ejercicio3/Main.java
@@ -1,9 +1,13 @@
 package AbstraccionYpolimorfismo.Ejercicio3;
 
 public class Main {
+
     public static void main(String[] args) {
-        Fraccion frac1 = new Fraccion(2,4);
+        Fraccion frac1 = new Fraccion(2, 4);
         Fraccion frac2 = new Fraccion(3, 4);
+
+        Numerica frac3 = frac1.sumar(frac2);
+        System.out.println(frac3.toString());
 
         System.out.println(frac1.sumar(frac2));
         System.out.println(frac1.restar(frac2));
