44571748f0dc5ce5a6fbdeaff7703c56073b3c40
Leonardo <leonardo38361@gmail.com>
2023-09-26 23:44:55 -0300

    clase Perros

--- src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/Perros.java
+++ src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/Perros.java
@@ -1,2 +1,33 @@
-package AbstraccionYpolimorfismo.Ejercicio2;public class Perros {
+package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;
+
+public class Perros extends Mascotas {
+    private int peso;
+    private boolean muerde;
+
+    public Perros(String nombre, int edad, String color, int peso, boolean muerde) {
+        super(nombre, edad, color);
+        this.peso = peso;
+        this.muerde = muerde;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public boolean isMuerde() {
+        return muerde;
+    }
+
+    public void setMuerde(boolean muerde) {
+        this.muerde = muerde;
+    }
+
+    @Override
+    public void sonido() {
+        System.out.printf("Los perros ladran");
+    }
 }
