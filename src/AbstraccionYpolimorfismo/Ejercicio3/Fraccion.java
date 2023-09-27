e36f762a77d79a002d8c9f25bbaec9113e62673c
Leonardo <leonardo38361@gmail.com>
2023-09-27 03:14:15 -0300

    más pruebas en ejercicio3

--- src/AbstraccionYpolimorfismo/Ejercicio3/Fraccion.java
+++ src/AbstraccionYpolimorfismo/Ejercicio3/Fraccion.java
@@ -9,6 +9,22 @@ public class Fraccion extends Numerica {
         this.denominador = denominador;
     }
 
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
     @Override
     public String toString() {
         return Integer.toString(this.numerador) + " / " + Integer.toString(this.denominador);
--- src/AbstraccionYpolimorfismo/Ejercicio3/Main.java
+++ src/AbstraccionYpolimorfismo/Ejercicio3/Main.java
@@ -9,11 +9,18 @@ public class Main {
         Numerica frac3 = frac1.sumar(frac2);
         System.out.println(frac3.toString());
 
-        System.out.println(frac1.sumar(frac2));
-        System.out.println(frac1.restar(frac2));
-        System.out.println(frac1.dividir(frac2));
+//        System.out.println(frac1.sumar(frac2));
+//        System.out.println(frac1.restar(frac2));
+//        System.out.println(frac1.dividir(frac2));
         System.out.println(frac1.multiplicar(frac2));
         System.out.println(frac1.equals(frac2));
+
+        Fraccion frac4 = new Fraccion(5, 9);
+
+
+        System.out.println("Resultado");
+        double result = (double) frac1.getNumerador() / frac1.getDenominador();
+        System.out.println(result);
 
 
     }
