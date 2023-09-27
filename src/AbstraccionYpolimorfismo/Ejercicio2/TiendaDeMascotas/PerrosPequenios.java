eddf8a7b4d49b90beb9289773da29db99de40f09
Leonardo <leonardo38361@gmail.com>
2023-09-26 23:45:53 -0300

    clase agregada

--- src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/PerrosPequenios.java
+++ src/AbstraccionYpolimorfismo/Ejercicio2/TiendaDeMascotas/PerrosPequenios.java
@@ -1,2 +1,10 @@
-package AbstraccionYpolimorfismo.Ejercicio2;public class PerrosPequenios {
+package AbstraccionYpolimorfismo.Ejercicio2.TiendaDeMascotas;
+
+public class PerrosPequenios extends Perros {
+    private String raza;
+
+    public PerrosPequenios(String nombre, int edad, String color, int peso, boolean muerde, String raza) {
+        super(nombre, edad, color, peso, muerde);
+        this.raza = raza;
+    }
 }
