3cd310b21dc89a337445cc9fadeca991fdb34959
Leonardo <leonardo38361@gmail.com>
2023-09-27 01:49:16 -0300

    Se agrega clase Numerica

--- src/AbstraccionYpolimorfismo/Ejercicio3/Numerica.java
+++ src/AbstraccionYpolimorfismo/Ejercicio3/Numerica.java
@@ -1,5 +1,10 @@
 package AbstraccionYpolimorfismo.Ejercicio3;
 
 public abstract class Numerica {
-    
+    public abstract String toString();
+    public abstract boolean equals(Object ob);
+    public abstract Numerica sumar(Numerica numero);
+    public abstract Numerica restar(Numerica numero);
+    public abstract Numerica multiplicar(Numerica numero);
+    public abstract Numerica dividir(Numerica numero);
 }
