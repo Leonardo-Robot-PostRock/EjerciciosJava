e216bc8b71edb8e719c7580ef4a887add06d6225
Leonardo <leonardo38361@gmail.com>
2023-10-03 18:14:00 -0300

    clase hija, metodos iniciales

--- src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoTerrestre.java
+++ src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoTerrestre.java
@@ -1,2 +1,21 @@
-package AbstraccionYpolimorfismo.Ejercicio4;public class VehiculoTerrestre {
+package AbstraccionYpolimorfismo.Ejercicio4;
+
+public class VehiculoTerrestre extends Vehiculo{
+    private int cantidadLlantas;
+    private String usoDelVehiculo;
+
+    @Override
+    public void acelerar() {
+
+    }
+
+    @Override
+    public void frenar() {
+
+    }
+
+    @Override
+    public void imprimir() {
+
+    }
 }
