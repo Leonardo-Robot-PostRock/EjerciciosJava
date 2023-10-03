66ff028f09ca2e4640beb2def30b7fbb1ed1bde3
Leonardo <leonardo38361@gmail.com>
2023-10-03 18:11:40 -0300

    creación de clase abstracta

--- src/AbstraccionYpolimorfismo/Ejercicio4/Vehiculo.java
+++ src/AbstraccionYpolimorfismo/Ejercicio4/Vehiculo.java
@@ -1,2 +1,11 @@
-package AbstraccionYpolimorfismo.Ejercicio4;public class Vehiculo {
+package AbstraccionYpolimorfismo.Ejercicio4;
+
+public abstract class Vehiculo {
+    private double velocidadActual;
+    private double velocidadMaxima;
+
+    public abstract void acelerar();
+    public abstract void frenar();
+    public abstract void imprimir();
+
 }
