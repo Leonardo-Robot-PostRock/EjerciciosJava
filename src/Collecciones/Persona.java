0605f4f8fdd9d7496508eb078d49568961cee53c
Leonardo <leonardo38361@gmail.com>
2023-10-03 19:34:23 -0300

    Ejemplo de collecciones

--- src/Collecciones/Persona.java
+++ src/Collecciones/Persona.java
@@ -1,2 +1,34 @@
-package Collecciones;public class Persona {
+package Collecciones;
+
+import java.util.ArrayList;
+
+public class Persona {
+    private String nombre;
+    private ArrayList<Domicilio> domicilios = new ArrayList<Domicilio>();
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void agregarDomicilio(Domicilio domi) {
+        domicilios.add(domi);
+    }
+
+    public void quitarDomicilio(Domicilio domi) {
+        domicilios.remove(domi);
+    }
+
+    public void mostrar() {
+        for (Domicilio domi : domicilios) {
+            System.out.println(domi.getCalle() + " " + domi.getNumero());
+        }
+    }
 }
