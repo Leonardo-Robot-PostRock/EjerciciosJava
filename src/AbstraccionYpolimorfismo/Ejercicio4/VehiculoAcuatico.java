0c9dc386777590ce53ab2172c91fe1e8c6b8a4c1
Leonardo <leonardo38361@gmail.com>
2023-10-03 18:13:46 -0300

    clase hija, metodos iniciales

--- src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoAcuatico.java
+++ src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoAcuatico.java
@@ -1,2 +1,22 @@
-package AbstraccionYpolimorfismo.Ejercicio4;public class VehiculoAcuatico {
+package AbstraccionYpolimorfismo.Ejercicio4;
+
+public class VehiculoAcuatico extends  Vehiculo{
+    private String tipo;
+    private int capacidadPasajeros;
+
+
+    @Override
+    public void acelerar() {
+
+    }
+
+    @Override
+    public void frenar() {
+
+    }
+
+    @Override
+    public void imprimir() {
+
+    }
 }
