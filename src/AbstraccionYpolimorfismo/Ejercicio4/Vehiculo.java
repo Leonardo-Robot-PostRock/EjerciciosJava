46ed4d1d5fc6c51cfc01be5372eb8277f94589da
Leonardo <leonardo38361@gmail.com>
2023-10-04 00:08:13 -0300

    Se crea clase Vehiculo

--- src/AbstraccionYpolimorfismo/Ejercicio4/Vehiculo.java
+++ src/AbstraccionYpolimorfismo/Ejercicio4/Vehiculo.java
@@ -1,11 +1,21 @@
 package AbstraccionYpolimorfismo.Ejercicio4;
 
 public abstract class Vehiculo {
-    private double velocidadActual;
-    private double velocidadMaxima;
+    protected double velocidadActual = 0;
+    protected double velocidadMaxima;
 
-    public abstract void acelerar();
-    public abstract void frenar();
-    public abstract void imprimir();
+    public Vehiculo(double velocidadActual, double velocidadMaxima) {
+        this.velocidadActual = velocidadActual;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public abstract void acelerar(double velocidad);
+
+    public abstract void frenar(double velocidad);
+
+    public void imprimir() {
+        System.out.println("Velocidad Actual: " + velocidadActual + " km/h");
+        System.out.println("Velocidad Máxima: " + velocidadMaxima + " km/h");
+    }
 
 }
