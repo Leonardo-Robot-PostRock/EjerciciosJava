e87f6f5bd933d5a7fe95581263983b90bcedc2a1
Leonardo <leonardo38361@gmail.com>
2023-10-04 00:09:02 -0300

    se termina Vehiculo acuatico

--- src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoAcuatico.java
+++ src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoAcuatico.java
@@ -1,22 +1,41 @@
 package AbstraccionYpolimorfismo.Ejercicio4;
 
-public class VehiculoAcuatico extends  Vehiculo{
+public class VehiculoAcuatico extends Vehiculo implements Vela {
     private String tipo;
     private int capacidadPasajeros;
 
-
-    @Override
-    public void acelerar() {
-
+    public VehiculoAcuatico(double velocidadActual, double velocidadMaxima, String tipo, int capacidadPasajeros) {
+        super(velocidadActual, velocidadMaxima);
+        this.tipo = tipo;
+        this.capacidadPasajeros = capacidadPasajeros;
     }
 
     @Override
-    public void frenar() {
-
+    public void acelerar(double velocidad) {
+        if (this.velocidadActual + velocidad <= velocidadMaxima) {
+            this.velocidadActual += velocidad;
+        } else {
+            System.out.println("No se puede incrementar a una velocidad superior a la máxima del automóvil");
+        }
     }
 
     @Override
-    public void imprimir() {
+    public void frenar(double velocidad) {
+        if (velocidadActual - velocidad > 0) {
+            velocidadActual -= velocidad;
+        } else {
+            System.out.println("No se puede decrementar a una velocidad negativa");
+        }
+    }
 
+    @Override
+    public void recomendarVelocidad(int velocidadViento) {
+        if (velocidadViento > 80) {
+            System.out.println("No es recomendable navegar con un viento de " + velocidadViento + "km/h");
+            frenar(velocidadActual);
+        } else if (velocidadViento < 10) {
+            System.out.println("No se recomienda navega con un viento de " + velocidadViento + "km/h");
+            frenar(velocidadViento);
+        }
     }
 }
