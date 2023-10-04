24a36df113220f50f1d3bca5dc8ee028275f54b2
Leonardo <leonardo38361@gmail.com>
2023-10-04 00:09:24 -0300

    se termina vehiculo terrestre

--- src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoTerrestre.java
+++ src/AbstraccionYpolimorfismo/Ejercicio4/VehiculoTerrestre.java
@@ -1,21 +1,51 @@
 package AbstraccionYpolimorfismo.Ejercicio4;
 
-public class VehiculoTerrestre extends Vehiculo{
+public class VehiculoTerrestre extends Vehiculo implements Motor {
     private int cantidadLlantas;
     private String usoDelVehiculo;
 
-    @Override
-    public void acelerar() {
+    public VehiculoTerrestre(double velocidadActual, double velocidadMaxima, int cantidadLlantas, String usoDelVehiculo) {
+        super(velocidadActual, velocidadMaxima);
+        this.cantidadLlantas = cantidadLlantas;
+        this.usoDelVehiculo = usoDelVehiculo;
+    }
 
+    public int getCantidadLlantas() {
+        return cantidadLlantas;
+    }
+
+    public void setCantidadLlantas(int cantidadLlantas) {
+        this.cantidadLlantas = cantidadLlantas;
+    }
+
+    public String getUsoDelVehiculo() {
+        return usoDelVehiculo;
+    }
+
+    public void setUsoDelVehiculo(String usoDelVehiculo) {
+        this.usoDelVehiculo = usoDelVehiculo;
     }
 
     @Override
-    public void frenar() {
-
+    public int calcularRevolucionesMotor(int fuerza, int radio) {
+        return fuerza * radio;
     }
 
     @Override
-    public void imprimir() {
+    public void acelerar(double velocidad) {
+        if (this.velocidadActual + velocidad <= velocidadMaxima) {
+            this.velocidadActual += velocidad;
+        } else {
+            System.out.println("No se puede incrementar a una velocidad superior a la máxima del automóvil");
+        }
+    }
 
+    @Override
+    public void frenar(double velocidad) {
+        if (velocidadActual - velocidad > 0) {
+            velocidadActual -= velocidad;
+        } else {
+            System.out.println("No se puede decrementar a una velocidad negativa");
+        }
     }
 }
