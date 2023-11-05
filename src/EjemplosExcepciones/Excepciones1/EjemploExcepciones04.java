89f0f14da8754bd3847c38574634abe8dbec88bd
Leonardo <leonardo38361@gmail.com>
2023-11-05 02:41:07 -0300

    mejorado de código con agregado de excepciones

--- src/EjemplosExcepciones/Excepciones1/EjemploExcepciones04.java
+++ src/EjemplosExcepciones/Excepciones1/EjemploExcepciones04.java
@@ -1,30 +1,43 @@
-package Excepciones1;
+package EjemplosExcepciones.Excepciones1;
 
 import java.util.Scanner;
 
 public class EjemploExcepciones04 {
-  public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-    System.out.println("Este programa pinta varias líneas de asteriscos");
+        int asteriscos = 0;
+        int lineas = 0;
 
-    System.out.print("Introduzca el número total de asteriscos: ");
-    int asteriscos = Integer.parseInt(s.nextLine());
+        System.out.println("Este programa pinta varias líneas de asteriscos");
+        try {
 
-    System.out.print("Introduzca el número de líneas que quiere pintar: ");
-    int lineas = Integer.parseInt(s.nextLine());
+            System.out.print("Introduzca el número total de asteriscos: ");
+            asteriscos = Integer.parseInt(s.nextLine());
 
-    int longitud = (asteriscos % lineas) == 0 ? asteriscos / lineas : (int)Math.ceil((double)asteriscos / lineas);
-    
-    int cuentaAsteriscos = 0;
+            System.out.print("Introduzca el número de líneas que quiere pintar: ");
+            lineas = Integer.parseInt(s.nextLine());
+        } catch (NumberFormatException ime) {
+            System.out.println("Error: Por favor, ingresa números enteros válidos");
+        }
 
-    for (int i = 1; i <= lineas; i++) {
-      System.out.print("Línea " + i + ": ");
-      for (int j = 0; (j < longitud) && (cuentaAsteriscos++ < asteriscos); j++) {
-        System.out.print("*");
-      }
-      System.out.println();
+        int longitud = 0;
+
+        try {
+            longitud = (asteriscos % lineas) == 0 ? asteriscos / lineas : (int) Math.ceil((double) asteriscos / lineas);
+        } catch (ArithmeticException ae) {
+            System.out.println("No se puede dividir por cero");
+        }
+
+        int cuentaAsteriscos = 0;
+
+        for (int i = 1; i <= lineas; i++) {
+            System.out.print("Línea " + i + ": ");
+            for (int j = 0; (j < longitud) && (cuentaAsteriscos++ < asteriscos); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
-
-  }
 }
