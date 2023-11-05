54e4b54674383d4c95d85abf2db3831e260e3e2e
Leonardo <leonardo38361@gmail.com>
2023-11-05 03:02:30 -0300

    manejo de excepcion con bloque try catch

--- src/EjemplosExcepciones/Excepciones1/EjemploExcepciones07SinThrow.java
+++ src/EjemplosExcepciones/Excepciones1/EjemploExcepciones07SinThrow.java
@@ -1,8 +1,12 @@
-package Excepciones1;
+package EjemplosExcepciones.Excepciones1;
 
 public class EjemploExcepciones07SinThrow {
-  public static void main(String[] args) {
-    System.out.println("Inicio");
-    System.out.println(1 / 0);
-  }
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+        try {
+            System.out.println(1 / 0);
+        } catch (ArithmeticException ae) {
+            System.out.println("Se ha producido un excepción: " + ae.getMessage() + " | no se puede dividir por cero");
+        }
+    }
 }
