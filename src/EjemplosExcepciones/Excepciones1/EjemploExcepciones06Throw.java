59d6a9df8619db2cdc94d4ef538a10edccead463
Leonardo <leonardo38361@gmail.com>
2023-11-05 00:18:14 -0300

    Excepciones ejemÃplos

---
+++ src/EjemplosExcepciones/Excepciones1/EjemploExcepciones06Throw.java
@@ -0,0 +1,8 @@
+package Excepciones1;
+
+public class EjemploExcepciones06Throw {
+  public static void main(String[] args) {
+    System.out.println("Inicio");
+    throw new ArithmeticException();
+  }
+}
