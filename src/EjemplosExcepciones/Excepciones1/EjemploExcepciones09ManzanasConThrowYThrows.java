fcbfac5bf220f0c34168a7a1d11ff6f280fb7dbc
Leonardo <leonardo38361@gmail.com>
2023-11-05 04:39:44 -0300

    mejorado de código y eliminacion de loop while

--- src/EjemplosExcepciones/Excepciones1/EjemploExcepciones09ManzanasConThrowYThrows.java
+++ src/EjemplosExcepciones/Excepciones1/EjemploExcepciones09ManzanasConThrowYThrows.java
@@ -1,27 +1,47 @@
-package Excepciones1;
+package EjemplosExcepciones.Excepciones1;
 
 import java.util.Scanner;
 
 public class EjemploExcepciones09ManzanasConThrowYThrows {
-  public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    System.out.print("Número de manzanas: ");
-    int m = Integer.parseInt(s.nextLine());
-    System.out.print("Número de personas: ");
-    int p = Integer.parseInt(s.nextLine());
-    try {
-      System.out.print("A cada persona le corresponden " + reparteManzanas(m, p) + " manzanas.");
-    } catch (ArithmeticException ae) {
-      System.out.println("Los datos introducidos no son correctos.");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m = 0;
+        int p = 0;
+
+        try {
+            System.out.print("Número de manzanas: ");
+            m = obtenerEnteroPositivo(s);
+            System.out.print("Número de personas: ");
+            p = obtenerEnteroPositivo(s);
+
+            int resultado = reparteManzanas(m, p);
+            System.out.println("A cada persona le corresponden " + resultado + " manzanas.");
+        } catch (EntradaInvalidaException | ArithmeticException eie) {
+            System.out.println("Error: " + eie.getMessage());
+        }
     }
-  }
-  
-  public static int reparteManzanas(int manzanas, int personas) throws ArithmeticException {
-    try {
-      return manzanas / personas;
-    } catch (ArithmeticException ae) {
-      System.out.println("El número de personas vale 0.");
-      throw ae;
+
+    public static int reparteManzanas(int manzanas, int personas) throws ArithmeticException {
+        if (personas == 0) {
+            throw new DivisionPorCeroException("El número de personas no puede ser 0.");
+        }
+
+        return manzanas / personas;
     }
-  }
+
+    public static int obtenerEnteroPositivo(Scanner scanner) {
+        int valor = 0;
+        while (true) {
+            try {
+                valor = Integer.parseInt(scanner.nextLine());
+                if (valor >= 0) {
+                    return valor;
+                } else {
+                    throw new EntradaInvalidaException("El número debe ser positivo.");
+                }
+            } catch (NumberFormatException nfe) {
+                throw new EntradaInvalidaException("La entrada no es un número válido.");
+            }
+        }
+    }
 }
