d68584a0a62350ddb93ff40dc1d93e20cd635ab5
Leonardo <leonardo38361@gmail.com>
2023-11-05 03:32:26 -0300

    Mejorado del código con excepciones

--- src/EjemplosExcepciones/Excepciones1/EjemploExcepciones08Manzanas.java
+++ src/EjemplosExcepciones/Excepciones1/EjemploExcepciones08Manzanas.java
@@ -1,18 +1,35 @@
-package Excepciones1;
+package EjemplosExcepciones.Excepciones1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EjemploExcepciones08Manzanas {
-  public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    System.out.print("Número de manzanas: ");
-    int m = Integer.parseInt(s.nextLine());
-    System.out.print("Número de personas: ");
-    int p = Integer.parseInt(s.nextLine());
-    System.out.print("A cada persona le corresponden " + reparteManzanas(m, p) + " manzanas.");
-  }
-  
-  public static int reparteManzanas(int manzanas, int personas) {
-    return manzanas / personas;
-  }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m = 0;
+        int p = 0;
+
+        try {
+            System.out.print("Número de manzanas: ");
+            m = Integer.parseInt(s.nextLine());
+
+            System.out.print("Número de personas: ");
+            p = Integer.parseInt(s.nextLine());
+
+            if (p == 0) {
+                throw new ArithmeticException("División por cero no permitida.");
+            }
+
+            double resultado = reparteManzanas(m, p);
+            System.out.println("A cada persona le corresponden " + resultado + " manzanas.");
+        } catch (NumberFormatException nfe) {
+            System.out.println("Error: Por favor, ingrese números enteros válidos.");
+        } catch (ArithmeticException ae) {
+            System.out.println("Error: " + ae.getMessage());
+        }
+    }
+
+    public static double reparteManzanas(int manzanas, int personas) {
+        return (double) manzanas / personas;
+    }
 }
