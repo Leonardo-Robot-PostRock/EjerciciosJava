f25ff20440eeed4aef528e9601515b7ee658f6e5
Leonardo <leonardo38361@gmail.com>
2023-11-05 02:55:01 -0300

    se captura el mensaje en un bloque try catch

--- src/EjemplosExcepciones/Excepciones1/EjemploExcepciones06Throw.java
+++ src/EjemplosExcepciones/Excepciones1/EjemploExcepciones06Throw.java
@@ -1,8 +1,12 @@
-package Excepciones1;
+package EjemplosExcepciones.Excepciones1;
 
 public class EjemploExcepciones06Throw {
   public static void main(String[] args) {
     System.out.println("Inicio");
-    throw new ArithmeticException();
+    try {
+      throw new ArithmeticException("Mensaje de error personalizado");
+    } catch (ArithmeticException ae) {
+      System.out.println("Se ha producido una excepción: " + ae.getMessage());
+    }
   }
 }
